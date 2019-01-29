de1d0a864b00a68f0f1c2692db79a25f7748055e
ritz-w <ritz.wu@outlook.com>
2019-01-29 16:07:32 +0800

    tried ommitting client from name

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(function(req, res, next){
             next()
         }
     })
-app.use(express.static('client/build'));
-
+    app.use(express.static(path.join(__dirname, 'build')));
+(console.log(__dirname))
 
 //configure database
 const db = require('./config/db');
@@ -41,9 +41,9 @@ mongoose.connect(db.url)
 
 require('./routes')(app);
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`we are live on port ${port}`)
